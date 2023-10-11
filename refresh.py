d0ce4c2c07d29c52d303700eb2c5053429ea1736
lvguanjun <1193965453@qq.com>
2023-10-12 01:29:39 +0800

    Separate request and Redis refresh logic
    1. 请求逻辑和 Redis 刷新逻辑分离
    2. 增加请求睡眠时间
    
    Signed-off-by: lvguanjun <1193965453@qq.com>

--- refresh.py
+++ refresh.py
@@ -7,6 +7,8 @@
 @Desc    :   refresh.py
 """
 
+import time
+
 import requests
 
 from config import PROXY, REFRESH_EXTRA_TIME
@@ -14,7 +16,7 @@ from custom_log import logger
 from redis_cache import get_need_refresh_tokens, set_to_redis
 
 
-def refresh_token():
+def get_new_token(refresh_token, session: requests.Session):
     base_url = "https://auth0.openai.com/oauth/token"
     headers = {"Content-Type": "application/json"}
     payload = {
@@ -23,32 +25,49 @@ def refresh_token():
         ),
         "grant_type": "refresh_token",
         "client_id": "pdlLIX2Y72MIl2rhLhTE9VV9bN905kBh",
+        "refresh_token": refresh_token,
     }
-    session = requests.Session()
     if PROXY:
         proxies = {"https": PROXY, "http": PROXY}
         session.proxies = proxies
-    for user, token in get_need_refresh_tokens(REFRESH_EXTRA_TIME):
-        refresh_token = token["refresh_token"]
-        payload["refresh_token"] = refresh_token
-        response = session.post(base_url, headers=headers, json=payload)
-        if response.status_code == 200:
-            update_token = response.json()
-            # 新获取的 token 不携带 refresh_token ，因此需要手动添加
-            update_token["refresh_token"] = refresh_token
-            set_to_redis(user, update_token)
-            logger.info(f"refresh token success, {user=}")
-        elif (
-            response.status_code == 403
-            and "Unknown or invalid refresh token." in response.text
-        ):
+    response = session.post(base_url, headers=headers, json=payload)
+    if response.status_code == 200:
+        update_token = response.json()
+        # 新获取的 token 不携带 refresh_token ，因此需要手动添加
+        update_token["refresh_token"] = refresh_token
+        return update_token
+    elif (
+        response.status_code == 403
+        and "Unknown or invalid refresh token." in response.text
+    ):
+        return {"change_password": True}
+    else:
+        logger.error(f"{response.status_code=}, {response.text=}")
+        return None
+
+
+def refresh_user_token(user, token, session: requests.Session):
+    refresh_token = token["refresh_token"]
+    update_token = get_new_token(refresh_token, session)
+    if update_token is not None:
+        if "refresh_token" not in update_token:
             token["change_password"] = True
             set_to_redis(user, token)
             logger.info(f"user change password, {user=}")
         else:
-            logger.error(f"{user=}, {response.status_code=}, {response.text=}")
-            continue
+            set_to_redis(user, update_token)
+            logger.info(f"refresh token success, {user=}")
+    else:
+        logger.error(f"refresh token failed, {user=}")
+
+
+def refresh_token(session: requests.Session, sleep_time: int):
+    for user, token in get_need_refresh_tokens(REFRESH_EXTRA_TIME):
+        refresh_user_token(user, token, session)
+        time.sleep(sleep_time)
 
 
 if __name__ == "__main__":
-    refresh_token()
+    sleep_time = 1
+    session = requests.Session()
+    refresh_token(session, sleep_time)
