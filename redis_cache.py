3c7dd2632e550478ee86e93971e4a2df0657997d
lvguanjun <lvguanjun135@foxmail.com>
2023-11-06 11:14:11 +0800

    Fix value error when key is expired

--- redis_cache.py
+++ redis_cache.py
@@ -127,7 +127,7 @@ def from_share_token_get_user(share_token: str, redis_cli=redis_cli) -> Optional
     for user in redis_cli.keys("*==*"):
         token = redis_cli.get(user)
         token = json.loads(token)
-        if token["share_token"] == share_token:
+        if token.get("share_token") == share_token:
             return user
     return None
 
