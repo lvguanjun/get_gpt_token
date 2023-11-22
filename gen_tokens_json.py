653f0a7c134fd6a9b21b38e9a66e3d066371b994
lvguanjun <lvguanjun135@foxmail.com>
2023-11-23 01:05:41 +0800

    Add gen no plus token json func

--- gen_tokens_json.py
+++ gen_tokens_json.py
@@ -7,9 +7,11 @@
 @Desc    :   gen_tokens_json.py
 """
 
+import datetime
 import json
 
-from redis_cache import redis_cli
+from config import DATETIME_FORMAT
+from redis_cache import gpt3_redis_cli, redis_cli
 
 
 def get_share_token_session_token_map():
@@ -29,6 +31,36 @@ def get_share_token_session_token_map():
             f" session_token={session_token is not None}"
         )
     return share_token_session_token_map
+
+
+def gen_tokens_json_not_plus():
+    tokens_json = {}
+    index = 1
+    for user in gpt3_redis_cli.keys("*==*"):
+        token = gpt3_redis_cli.get(user)
+        token = json.loads(token)
+        expire_time = token["expired_time"]
+        expire_time = datetime.datetime.strptime(expire_time, DATETIME_FORMAT)
+        if datetime.datetime.now() > expire_time:
+            continue
+        if all(
+            [
+                not token.get("deactivated"),
+                not token.get("change_password"),
+                token.get("session_token"),
+            ]
+        ):
+            key = f"user-{index // 100 % 10}-{index // 10 % 10}-{index}"
+            tokens_json[key] = {
+                "token": token["session_token"],
+                "shared": True,
+                "show_user_info": True,
+                "plus": False,
+            }
+            index += 1
+            if index == 101:
+                break
+    return tokens_json
 
 
 def gen_tokens_json(share_tokens: list[str], share_token_session_token_map: dict):
@@ -51,5 +83,6 @@ if __name__ == "__main__":
         share_tokens = json.load(f)
     share_token_session_token_map = get_share_token_session_token_map()
     tokens_json = gen_tokens_json(share_tokens, share_token_session_token_map)
+    # tokens_json = gen_tokens_json_not_plus()
     with open(tokens_json_file, "w") as f:
         json.dump(tokens_json, f, indent=4)
